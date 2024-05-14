5046a0fe5a58641c17bf7e4695adf654d612cb16
Gaetan Brasseur <gaetan1.brasseur@gmail.com>
2024-05-14 15:29:37 +0200

    Squelette html commencé, reste a décider des différentes pages
    ATTENTION : Modification du settings.py pour
    les differentes URL
    Non prises en compte dans le git car le settings.py
    est dans le gitignore
    
    Ajouter/remplacer ceci a la fin du code :
    
    import os
    STATIC_ROOT = os.path.join(BASE_DIR, 'bdd_icono','static')
    STATIC_URL = 'static/'
    
    MEDIA_ROOT = os.path.join(BASE_DIR,'bdd_icono', 'media')
    MEDIA_URL = 'media/'

--- projet_ricercar/bdd_icono/views.py
+++ projet_ricercar/bdd_icono/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from bdd_icono.models import Image
+from django.views import generic
 
-# Create your views here.
+def home(request):
+    img_count = Image.objects.count()
+    context = {
+        'nombre_image' : img_count
+    }
+    return render(request, 'bdd_icono/home.html',context)
