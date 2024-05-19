a1c1b5f8090119eaabff7479db786e910029505e
Gaetan Brasseur <gaetan1.brasseur@gmail.com>
2024-05-19 16:18:28 +0200

    Modification du settings.py, lire le readme + ajout des pages resultats et image (non mises en formes)

--- projet_ricercar/bdd_icono/views.py
+++ projet_ricercar/bdd_icono/views.py
@@ -12,3 +12,28 @@ def home(request):
 
 def inProgress(request):
     return render(request, 'bdd_icono/in_progress.html')
+
+
+def recherche(request):
+    return render(request, 'bdd_icono/recherche.html')
+
+def resultats(request):
+    images = Image.objects.all
+    
+    context = {
+        'images' : images
+    }
+    return render(request, 'bdd_icono/resultats.html',context)
+
+def image(request, id_image):
+    from django.http import Http404
+
+    try :
+        image = Image.objects.get(id=id_image)
+    except Image.DoesNotExist:
+        raise Http404("Aucun auteur trouvé pour cet identifiant")
+    context = {
+        'image' : image
+    }
+
+    return render(request, 'bdd_icono/image.html', context)
