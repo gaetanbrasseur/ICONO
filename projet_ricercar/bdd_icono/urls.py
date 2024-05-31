425d1dd39d5677fef0334722590fc8f44c462067
Youna <164154355+younafroger@users.noreply.github.com>
2024-05-31 18:01:01 +0200

    ajout url pour télécharger l'image

--- projet_ricercar/bdd_icono/urls.py
+++ projet_ricercar/bdd_icono/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from bdd_icono import views
 from django.conf import settings
 from django.conf.urls.static import static
-# Create your views here.
+from .views import download_image
+
+
 urlpatterns =[
     path('',views.home, name="home"),
     path('inprogress/', views.inProgress, name='in_progress'),
     path('recherche/', views.recherche, name='recherche'),
     path('resultats/', views.resultats, name='resultats'),
     path('image/<int:id_image>/', views.image, name='image'),
+    path('download/<int:image_id>/', download_image, name='download_image'),
 ]
 
 if settings.DEBUG:
