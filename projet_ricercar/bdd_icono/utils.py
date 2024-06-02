5845f5b88957a1b9999697edbef5cec436feae32
Youna <164154355+younafroger@users.noreply.github.com>
2024-06-02 15:50:36 +0200

    ajout fonction miniature

--- projet_ricercar/bdd_icono/utils.py
+++ projet_ricercar/bdd_icono/utils.py
@@ -1,14 +1,15 @@
 from PIL import Image as PILImage, UnidentifiedImageError
 import os
 
-def miniatures(image_path, output_dir):
+
+def miniatures(image_dossier, chemin_miniatures):
     """
-    Génère une miniature basse définition pour une image et l'enregistre dans output_dir.
+    Génère une miniature pour une image et l'enregistre dans chemin_miniatures.
     """
     basse_resolution = (800, 600)
     
     try:
-        image = PILImage.open(image_path)
+        image = PILImage.open(image_dossier)
         largeur, hauteur = image.size
         ratio = largeur / hauteur
 
@@ -21,10 +22,10 @@ def miniatures(image_path, output_dir):
 
         image_bassedef = image.resize((nouvelle_largeur, nouvelle_hauteur), PILImage.Resampling.LANCZOS)
 
-        nom_fichier_sans_ext = os.path.splitext(os.path.basename(image_path))[0]
-        thumbnail_path = os.path.join(output_dir, f"{nom_fichier_sans_ext}.jpg")
+        nom_fichier_sans_ext = os.path.splitext(os.path.basename(image_dossier))[0]
+        dossier_miniature = os.path.join(chemin_miniatures, f"{nom_fichier_sans_ext}.jpg")
 
-        image_bassedef.save(thumbnail_path, "JPEG")
+        image_bassedef.save(dossier_miniature, "JPEG")
 
     except (UnidentifiedImageError, IOError) as e:
         print(f"Erreur lors de l'ouverture de {image_path}: {e}")
